9b77a7fd69b07724edb4664976e1105773fb9731
ozluuh <ozluuh@gmail.com>
2021-03-01 10:09:30 -0300

    Exercicio02

---
+++ Exercicio02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercicio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i <= 215; i++)
+            {
+                if (i % 3 == 0 && i % 7 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+        }
+    }
+}
