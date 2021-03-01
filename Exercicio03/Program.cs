f3d248e3552950f9785d17f936050c125c7cb175
ozluuh <ozluuh@gmail.com>
2021-03-01 10:24:06 -0300

    Exercicio03

---
+++ Exercicio03/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exercicio03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe intervalo Inicial:");
+            int intervaloInicial = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Informe intervalo Final:");
+            int intervaloFinal = Int32.Parse(Console.ReadLine());
+
+            int intervalo = intervaloFinal - intervaloInicial;
+
+            if (intervalo <= 15)
+            {
+                for (int i = intervaloInicial; i <= intervaloFinal; i++)
+                {
+                    Console.WriteLine($"Cubo de {i} = {Math.Pow(i, 3)}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Fim da execução, intervalo não pode ser superior a 15");
+                Console.WriteLine($"Intervalo atual é {intervalo}");
+            }
+
+        }
+    }
+}
